228e2f3adf1947eb919992a1fd5f28e9b09afcad
Yichi Zhang <yichizhang@Yichis-MacBook-Pro.local>
2024-09-21 22:02:36 -0700

    update TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -41,7 +41,11 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/AliciaZYC" className="nav-link">
+        <a
+          id="wd-github"
+          href="https://github.com/AliciaZYC"
+          className="nav-link"
+        >
           My GitHub
         </a>
       </li>
